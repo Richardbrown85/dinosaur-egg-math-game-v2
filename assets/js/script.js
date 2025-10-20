20ca23de96815fcbd4a2bec0de2fcc677654bedc
Richardbrown85 <richard.s.brown@live.co.uk>
2025-10-20 21:40:48 +0100

    deleted extra  in level up message

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,7 +82,7 @@ function checkLevelUp() {
 
 function showLevelUpModal() {
     updateDifficultyDisplay();
-    levelUpMessage.textContent = `Wow! You made it to Level 2—awesome work!` ${difficultyLevel}!`;
+    levelUpMessage.textContent = `Wow! You made it to Level 2—awesome work! ${difficultyLevel}!`;
     levelUpModal.classList.add('show');  
 }
 
